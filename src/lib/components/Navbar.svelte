450ef1ea65576418aa946498664eead62136ce74
Jxk0be <jakeshoffner27@gmail.com>
2024-10-14 18:48:26 -0400

    Refactor mobile menu to be smaller

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -54,26 +54,7 @@
 
 <svelte:window bind:innerWidth={screenWidth} />
 
-<div class={`${$isMenuOpen ? 'translate-y-0' : 'translate-y-[-100%]'} w-full text-white text-3xl font-semibold duration-[670ms] flex ease-in-out justify-center items-center flex-col lg:hidden transition-all h-full bg-[#212121] z-[5] fixed right-0`}>
-    <button class={`${$isMenuOpen ? 'translate-y-0' : 'translate-y-[50%]'} fixed top-0 text-[35px] duration-[670ms] transitiona-all ease-in-out right-0 h-[70px] flex items-center pr-[16px]`} on:click={handleMenu}>
-        <Icon icon="ep:close-bold" />
-    </button>
-
-    <button on:click={() => awaitNav("/")} class="my-3 capitalize">Home</button>
-    {#each jsonData?.links as link, idx}
-        {#if idx === jsonData?.links?.length - 1}
-            <button on:click={() => awaitNav(link)} class="my-3 capitalize">{link}</button>
-        {:else}
-            {#if link.toLowerCase() === "our work"}
-                <button on:click={() => awaitNav("our-work")} class="my-3 capitalize">{link}</button>
-            {:else}
-                <button on:click={() => awaitNav(link)} class="my-3 capitalize">{link}</button>
-            {/if}
-        {/if}
-    {/each}
-</div>
-
-<div class={`w-full text-white z-[3] font-semibold fixed h-[70px] bg-[#212121] border-b-[1px] border-black/10`}>
+<div class={`w-full text-white z-[4] font-semibold fixed h-[70px] bg-[#212121] border-b-[1px] border-black/10`}>
     <div class="lg:px-[24px] relative z-[4] px-[16px] h-full w-full flex items-center justify-between">
         <button on:click={() => awaitNav("/")} class="cursor-pointer w-[100px]">{@html LogoText}</button>
 
@@ -81,8 +62,11 @@
             <button on:click={handleMenu} class={`lg:hidden text-[35px] text-white transition-all ease-in-out rounded-xl duration-150`}>
                 <Icon icon="lucide:menu" />
             </button>
+        {:else}
+            <button on:click={handleMenu} class={`lg:hidden text-[35px] text-white transition-all ease-in-out rounded-xl duration-150`}>
+                <Icon icon="ep:close-bold" />
+            </button>
         {/if}
-
 
         <div class="hidden lg:flex w-auto items-center justify-between">
             <button on:click={() => awaitNav("/")} class="mr-8">Home</button>
@@ -99,4 +83,19 @@
             {/each}
         </div>
     </div>
+</div>
+
+<div class={`${$isMenuOpen ? 'translate-y-[26%]' : 'translate-y-[-80%]'} fixed right-0 z-[3] text-xl font-semibold duration-[200ms] ease-in-out flex justify-center items-center flex-col transition-all lg:hidden bg-[#212121] text-white w-full`}>
+    <button on:click={() => awaitNav("/")} class="py-3 w-full border-b-[1px] border-white/10 capitalize">Home</button>
+    {#each jsonData?.links as link, idx}
+        {#if idx === jsonData?.links?.length - 1}
+            <button on:click={() => awaitNav(link)} class="py-3 w-full capitalize">{link}</button>
+        {:else}
+            {#if link.toLowerCase() === "our work"}
+                <button on:click={() => awaitNav("our-work")} class="w-full border-b-[1px] border-white/10 py-3 capitalize">{link}</button>
+            {:else}
+                <button on:click={() => awaitNav(link)} class="w-full border-b-[1px] border-white/10 py-3 capitalize">{link}</button>
+            {/if}
+        {/if}
+    {/each}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,14 +34,15 @@
 <svelte:window bind:innerWidth={screenWidth} />
 
 <div class="text-white w-full h-screen md:h-[70vh] flex flex-col items-center justify-center relative">
-    <h1 class="text-3xl z-[2] md:w-[400px] md:max-w-[500px] max-w-[400px] w-3/4">{@html LogoText}</h1>
-    <h1 class="text-xl z-[2] capitalize">automotive liquid wrapping</h1>
+    <h1 class="text-3xl z-[2] md:w-[400px] md:max-w-[500px] min-w-[315px] max-w-[400px] w-3/4">{@html LogoText}</h1>
+    <h1 class="text-xl z-[2] capitalize whitespace-nowrap">automotive liquid wrapping</h1>
     <div class="md:w-[400px] px-[16px] md:px-0 mt-4 z-[2] gap-3 w-full flex justify-between items-center h-[40px]">
-        <button on:click={() => awaitNav("/quote")} class="h-full w-1/2 uppercase rounded-md border-slate-100 border-[2px] bg-transparent">Get a free quote</button>
-        <a href="#about" class="h-full w-1/2 flex items-center justify-center uppercase rounded-md border-slate-100 border-[2px] bg-transparent">Learn more</a>
+        <button on:click={() => awaitNav("/quote")} class="h-full min-w-[156px] w-1/2 uppercase rounded-md border-slate-100 border-[2px] bg-transparent">Get a free quote</button>
+        <a href="#about" class="h-full min-w-[156px] w-1/2 flex items-center justify-center uppercase rounded-md border-slate-100 border-[2px] bg-transparent">Learn more</a>
     </div>
     <img class="absolute top-0 right-0 z-[1] w-full object-cover md:h-[70vh] h-screen" src={HeroImg} alt="carHero" />
 </div>
+
 <div use:reveal={{ transition: "slide", blur: 20, x: -50, duration: 1200}} class="bg-[#4090ff] flex-col px-[16px] md:px-0 w-full h-screen flex items-center">
     <div id="about" class="w-full h-[100px]">&nbsp;</div>
     <h1 class="text-3xl font-bold">About Fusion Auto Lab</h1> 
