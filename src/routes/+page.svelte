390f7eed7eb20d1f71ca016f8870e85103ecdf38
Jxk0be <jakeshoffner27@gmail.com>
2024-10-16 19:52:19 -0400

    Fixing mobile viewport and some UI changes for carousel slides

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,32 +38,44 @@
 
 <svelte:window bind:innerWidth={screenWidth} />
 
-<div class="text-white w-full h-[480px] flex flex-col items-center justify-center relative lg:px-[24px] md:px-[16px]">
+<div class="text-white w-full h-[500px] mb-[30px] flex flex-col items-center justify-center relative lg:px-[24px] md:px-[16px]">
     <h1 class="text-3xl z-[2] md:w-[300px] w-[250px]">{@html LogoText}</h1>
     <h1 class="text-xl z-[2] capitalize whitespace-nowrap">automotive liquid wrapping</h1>
-    <div class="md:w-[400px] md:min-w-[500px] px-[16px] md:px-0 mt-4 z-[2] gap-3 w-full flex justify-between items-center h-[40px]">
+    <div class="md:w-[400px] md:min-w-[500px] max-w-[400px] px-[16px] md:px-0 mt-4 z-[2] w-full flex justify-center items-center h-[40px]">
         <button on:click={() => awaitNav("/quote")} class="h-full min-w-[156px] w-1/2 text-nowrap uppercase rounded-md border-slate-100 border-[2px] bg-transparent">Get a free quote</button>
-        <a href="#about" class="h-full min-w-[156px] w-1/2 flex items-center justify-center text-nowrap uppercase rounded-md border-slate-100 border-[2px] bg-transparent">Learn more</a>
     </div>
-    <img class="absolute top-0 right-0 z-[1] w-full object-cover h-[480px]" src={HeroImg} alt="carHero" />
+    <img class="absolute top-0 right-0 z-[1] w-full object-cover h-[500px]" src={HeroImg} alt="carHero" />
 </div>
 
-<div use:reveal={{ transition: "slide", blur: 20, x: -50, duration: 1200}} class="lg:px-[24px] md:px-[16px] flex-col w-full h-screen flex items-center">
-    <div id="about" class="w-full h-[100px]">&nbsp;</div>
+<div use:reveal={{ transition: "slide", blur: 20, x: -50, duration: 1200}} class="lg:px-[24px] md:px-[16px] flex-col w-full h-[500px] flex items-center">
     <h1 class="text-3xl font-[500] text-center">About Fusion Auto Lab</h1> 
 
     {#if browser}
-        <div class="lg:px-[24px] select-none md:flex flex-col hidden md:px-[16px] max-w-[1200px] max-h-[800px] h-[500px]">
+        <div class="lg:px-[24px] select-none flex flex-col px-[16px] w-full max-w-[1140px] h-full">
             <Carousel bind:this={carousel}
             autoplay autoplayDuration={4000} arrows={false}
             >
-                <div>Hello</div>
-                <div>Testing</div>
+                <div class="flex md:flex-row flex-col h-[400px] w-full justify-center md:justify-between items-center">
+                    <h1>Service 1 & Explanation</h1>
+                    <h1>Imagine an Image here</h1>
+                </div>
+                <div class="flex md:flex-row flex-col h-[400px] w-full justify-center md:justify-between items-center">
+                    <h1>Service 2 & Explanation</h1>
+                    <h1>Imagine an Image here</h1>
+                </div>
+                <div class="flex md:flex-row flex-col h-[400px] w-full justify-center md:justify-between items-center">
+                    <h1>Service 3 & Explanation</h1>
+                    <h1>Imagine an Image here</h1>
+                </div>
             </Carousel>
     
             <button on:click={handleNextClick}>Next</button>
         </div>
     {/if}
+
+</div>
+
+<div class="lg:px-[24px] md:px-[16px] flex-col w-full h-[400px] mt-[30px] flex items-center bg-slate-100">
 
 </div>
 
